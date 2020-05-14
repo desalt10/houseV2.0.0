b8b0f093395f3fa0bfb9ed16d8148d5677b1332f
desalt10 <648377611@qq.com>
2020-05-14 16:32:59 +0800

    page5-css

--- js/Receive12.js
+++ js/Receive12.js
@@ -1,20 +1,23 @@
 /**
  * 功能: 处理接收函数Receive12()-Receive19 智能插座 @0012-0019
- * @param str10  接收需要处理数据的对象
- * @param tem 温度
- * @param hum 湿度
- * 对象.CurrentTemperature  温度
- * 对象.CurrentHumidity  湿度
+ * 
  */
-var tem,hum
 function Receive12(obj12){
-	if(obj12=="1"){
-		$("#img1").attr("src","02.png");
-	}
+	ChangeImg(obj12)
 }
-function Get11(s){
-	tem = s.CurrentTemperature;
-	hum = s.CurrentHumidity;
-	$("#tem").text(tem+"(℃)");
-	$("#hum").text(hum+"(%)");
+function Receive13(obj13){
+	ChangeImg(obj13)
+}
+function Receive14(obj14){
+	ChangeImg(obj13)
+}
+function ChangeImg(s){
+	for(var i=1;i<9;i++){
+		if(s["PowerSwitch_"+i]=="1"){
+			$("#img"+i).attr("src", "../../img/page12-on.png");
+		}
+		if(s["PowerSwitch_"+i]=="0"){
+			$("#img"+i).attr("src", "../../img/page12-off.png");
+		}
+	}
 }
